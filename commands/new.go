80faf2840b174625c623f07487baa3c43b45d129
Shiranui Nui <shiranuisayaka@gmail.com>
2020-06-13 22:08:26 +0900

    [Fix] 自分のコールサインをconfigファイルから取得する様に修正

--- commands/new.go
+++ commands/new.go
@@ -11,47 +11,65 @@ import (
 	"github.com/ShiranuiNui/HamRadioCliLogger/models"
 	tw "github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
 )
 
 func init() {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:     "callsign",
+			Usage:    "callsign of worked station",
+			Aliases:  []string{"c"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "report",
+			Usage:    "sent report like rst",
+			Aliases:  []string{"r"},
+			Required: true,
+		},
+		&cli.IntFlag{
+			Name:     "freq",
+			Usage:    "frequency",
+			Aliases:  []string{"f"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "mode",
+			Usage:    "mode",
+			Aliases:  []string{"m"},
+			Required: true,
+		},
+		&cli.BoolFlag{
+			Name:    "isRequestedQSLCard",
+			Usage:   "Worked Station needs QSL Card?",
+			Value:   false,
+			Aliases: []string{"q"},
+		},
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:     "mycallsign",
+			Usage:    "mycallsign",
+			Aliases:  []string{"mc"},
+			Required: true,
+		}),
+	}
 	CmdList = append(CmdList, &cli.Command{
 		Name:  "new",
 		Usage: "Create New QSO Logging",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "callsign",
-				Usage:    "callsign of worked station",
-				Aliases:  []string{"c"},
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "report",
-				Usage:    "sent report like rst",
-				Aliases:  []string{"r"},
-				Required: true,
-			},
-			&cli.IntFlag{
-				Name:     "freq",
-				Usage:    "frequency",
-				Aliases:  []string{"f"},
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "mode",
-				Usage:    "mode",
-				Aliases:  []string{"m"},
-				Required: true,
-			},
-			&cli.BoolFlag{
-				Name:    "isRequestedQSLCard",
-				Usage:   "Worked Station needs QSL Card?",
-				Value:   false,
-				Aliases: []string{"q"},
-			},
-		},
+		Flags: flags,
+		Before: altsrc.InitInputSource(flags, func() (altsrc.InputSourceContext, error) {
+			usr, _ := user.Current()
+			if _, err := os.Stat(usr.HomeDir + "/.config/hamradio_logger"); os.IsNotExist(err) {
+				os.Mkdir(usr.HomeDir+"/.config/hamradio_logger", 0777)
+			}
+			if _, err := os.Stat(usr.HomeDir + "/.config/hamradio_logger/config.yaml"); os.IsNotExist(err) {
+				return &altsrc.MapInputSource{}, nil
+			}
+			return altsrc.NewYamlSourceFromFile(usr.HomeDir + "/.config/hamradio_logger/config.yaml")
+		}),
 		Action: func(c *cli.Context) error {
 			time := models.ISO8601Time{Time: time.Now()}
-			qso := models.QSO{MyCallSign: "JM8HBM", CallSign: c.String("callsign"), Time: time, Report: c.String("report"), Frequency: c.Int("freq"), Mode: c.String("mode"), IsRequestedQSLCard: c.Bool("isRequestedQSLCard")}
+			qso := models.QSO{MyCallSign: c.String("mycallsign"), CallSign: c.String("callsign"), Time: time, Report: c.String("report"), Frequency: c.Int("freq"), Mode: c.String("mode"), IsRequestedQSLCard: c.Bool("isRequestedQSLCard")}
 			table := tw.NewWriter(os.Stdout)
 			table.SetAutoFormatHeaders(false)
 			table.SetHeader([]string{"MyCallSign", "CallSign", "Time", "Report", "Frequency", "Mode", "IsRequestedQSLCard"})
@@ -85,7 +103,9 @@ func init() {
 
 func writeJSON(json []byte) error {
 	usr, _ := user.Current()
-	os.Mkdir(usr.HomeDir+"/.hamradio_logger", 0777)
+	if _, err := os.Stat(usr.HomeDir + "/.hamradio_logger"); os.IsNotExist(err) {
+		os.Mkdir(usr.HomeDir+"/.hamradio_logger", 0777)
+	}
 	fp, err := os.OpenFile(usr.HomeDir+"/.hamradio_logger/log.jsonl", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0664)
 	if err != nil {
 		return err
